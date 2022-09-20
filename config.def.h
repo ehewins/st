5cd434d0d21f9c284302072a62c5ff31008c6b2c
Ellis Hewins <ellishewins@gmail.com>
2022-09-20 19:27:25 +0100

    Added Luke Smith's copyoutput, copyurlcmd and openurlcmd capabilities

--- config.def.h
+++ config.def.h
@@ -238,8 +238,12 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD2 (Mod1Mask|ShiftMask)
+
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
+static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", "externalpipe", NULL };
+static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -252,12 +256,15 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	//{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
     { TERMMOD,              XK_Return,      newterm,        {.i =  0} },
+	{ TERMMOD2,             XK_L,           externalpipe,   {.v = openurlcmd } },
+	{ TERMMOD2,             XK_Y,           externalpipe,   {.v = copyurlcmd } },
+	{ TERMMOD2,             XK_O,           externalpipe,   {.v = copyoutput } },
 };
 
 /*
